36228d4d73df96bb7d6615179a284064eff7750b
Fabio Alicante <alicantef@gmail.com>
2022-08-20 09:46:52 +0200

    200 events block

--- shipLHC/scripts/emulsionMatch.py
+++ shipLHC/scripts/emulsionMatch.py
@@ -1,48 +1,127 @@
 import ROOT
 
-
-c = ROOT.TCanvas('residuals', 'residuals', 1200, 1200)
-c.Divide(1, 2)
-resX = ROOT.TH1D('resX', 'residuals x', 100, -20, 20)
-resY = ROOT.TH1D('resY', 'residuals y', 100, -20, 20)
-
 path = '/home/fabio/Simulations/numu_sim_25_July_2022/'
 scifiFile = path+'scifi_fits.txt'
 emulsionFile = path+'eml_fits.txt'
-with open(emulsionFile, 'r') as fe:
-    linesEml = fe.readlines()
-    for lineEml in linesEml:
-        lineEml = lineEml.replace('\n', '')
-        lineEml = lineEml.split()
-        if lineEml[0] == 'event': continue
-        emulsionEvent = int(lineEml[0])
-        emulsionBarX = float(lineEml[1])
-        emulsionBarY = float(lineEml[3])
-        with open (scifiFile, 'r') as fs:
-            linesScifi = fs.readlines()
-            for lineScifi in linesScifi:
-                lineScifi = lineScifi.replace('\n', '')
-                lineScifi = lineScifi.split()
-                if lineScifi[0] == 'event': continue
-                scifiEvent = int(lineScifi[0])
-                if scifiEvent == emulsionEvent:
+
+def allEvents():
+    c = ROOT.TCanvas('residuals', 'residuals')
+    c.Divide(1, 3)
+    c.SetCanvasSize(1000, 1500)
+    c.SetWindowSize(1050, 1200)
+    resX = ROOT.TH1D('resX', 'residuals x', 100, -20, 20)
+    resY = ROOT.TH1D('resY', 'residuals y', 100, -20, 20)
+    resD = ROOT.TH1D('resD', 'residuals distance', 100, 0, 20)
+
+    with open(emulsionFile, 'r') as fe:
+        linesEml = fe.readlines()
+        for lineEml in linesEml:
+            lineEml = lineEml.replace('\n', '')
+            lineEml = lineEml.split()
+            if lineEml[0] == 'event': continue
+            emulsionEvent = int(lineEml[0])
+            emulsionBarX = float(lineEml[1])
+            emulsionBarY = float(lineEml[3])
+            with open (scifiFile, 'r') as fs:
+                linesScifi = fs.readlines()
+                for lineScifi in linesScifi:
+                    lineScifi = lineScifi.replace('\n', '')
+                    lineScifi = lineScifi.split()
+                    if lineScifi[0] == 'event': continue
+                    scifiEvent = int(lineScifi[0])
+                    if scifiEvent == emulsionEvent:
+                        scifiBarX = float(lineScifi[1])
+                        scifiBarY = float(lineScifi[3])
+                        resX.Fill(emulsionBarX-scifiBarX)
+                        resY.Fill(emulsionBarY-scifiBarY)
+                        dist = ROOT.TMath.Sqrt((emulsionBarX-scifiBarX)**2+(emulsionBarY-scifiBarY)**2)
+                        resD.Fill(dist)
+    c.cd(1)
+    fitResX = resX.Fit('gaus','SQ','',-10,10)
+    fitResY = resY.Fit('gaus','SQ','',-10,10)
+    resMeanX = fitResX.Parameter(1)
+    resVarX = fitResX.Parameter(2)
+    resMeanY = fitResY.Parameter(1)
+    resVarY = fitResY.Parameter(2)
+    eff0x = 0
+    eff0y = 0
+    eff1x = resX.GetEntries()
+    eff1y = resY.GetEntries()
+    for binX in range(resX.GetXaxis().GetNbins()):
+        if resX.GetBinCenter(binX) < resMeanX - 3 * resVarX:
+            eff0x += resX.GetBinContent(binX)
+        elif resX.GetBinCenter(binX) > resMeanX + 3 * resVarX:
+            eff0x += resX.GetBinContent(binX)
+    for binY in range(resY.GetXaxis().GetNbins()):
+        if resY.GetBinCenter(binY) < resMeanY - 3 * resVarY:
+            eff0y += resY.GetBinContent(binY)
+        elif resY.GetBinCenter(binY) > resMeanY + 3 * resVarY:
+            eff0y += resY.GetBinContent(binY)
+    effx = 1-eff0x/eff1x
+    effy = 1-eff0y/eff1y
+    print('efficiency x:', effx, eff0x, eff1x)
+    print('efficiency y:', effy, eff0y, eff1y)
+    resX.Draw()
+    c.cd(2)
+    resY.Draw()
+    c.cd(3)
+    resD.Draw()
+    print(resMeanX, resVarX, resMeanY, resVarY)
+
+    c.Print('/home/fabio/Simulations/numu_sim_25_July_2022/eml_scifi_2.png')
+
+def blockEvents(Nint = 200):
+    #c = ROOT.TCanvas('resid', 'residuals', 1280, 720)
+    #distT = ROOT.TH1D('distT', 'true event distance', 10, 0, 1)
+    #distF = ROOT.TH1D('distF', 'false event distance', 10, 0, 1)
+    tCount = 0
+    fCount = 0
+    N = 0
+    block = 0
+    with open(emulsionFile, 'r') as fe:
+        linesEml = fe.readlines()
+        for lineEml in linesEml:
+            lineEml = lineEml.replace('\n', '')
+            lineEml = lineEml.split()
+            if lineEml[0] == 'event': continue
+            N+=1
+            emulsionEvent = int(lineEml[0])
+            emulsionBarX = float(lineEml[1])
+            emulsionBarY = float(lineEml[3])
+            minDist = 100
+            with open (scifiFile, 'r') as fs:
+                linesScifi = fs.readlines()
+                for lineScifi in linesScifi:
+                    lineScifi = lineScifi.replace('\n', '')
+                    lineScifi = lineScifi.split()
+                    if lineScifi[0] == 'event': continue
                     scifiBarX = float(lineScifi[1])
                     scifiBarY = float(lineScifi[3])
-                    resX.Fill(emulsionBarX-scifiBarX)
-                    resY.Fill(emulsionBarY-scifiBarY)
-c.cd(1)
-fitResX = resX.Fit('gaus','SQ','',-10,10)
-fitResY = resY.Fit('gaus','SQ','',-10,10)
-resMeanX = fitResX.Parameter(1)
-resVarX = fitResX.Parameter(2)
-resMeanY = fitResY.Parameter(1)
-resVarY = fitResY.Parameter(2)
-resX.Draw()
-c.cd(2)
-resY.Draw()
-print(resMeanX, resVarX, resMeanY, resVarY)
+                    dist = ROOT.TMath.Sqrt((emulsionBarX-scifiBarX)**2+(emulsionBarY-scifiBarY)**2)
+                    if dist < minDist:
+                        minDist = dist
+                        scifiEvent = int(lineScifi[0])
+                if scifiEvent == emulsionEvent:
+                    #distT.Fill(minDist)
+                    tCount+=1
+                else:
+                    #distF.Fill(minDist)
+                    fCount+=1
+            if N == Nint: 
+                N = 0
+                block+=1
+                #tCount = distT.GetEntries()
+                #fCount = distF.GetEntries()
+                print('block {} true {} false {}'.format(block, tCount, fCount))
+                print('block {} matching efficiency {}'.format(block, tCount/(tCount+fCount)))
+                tCount = 0
+                fCount = 0
 
-c.Print('/home/fabio/Simulations/numu_sim_25_July_2022/eml_scifi_2.png')
 
-
-
+    
+    #distT.SetMaximum(100)
+    #distT.SetLineColor(4)
+    #distF.SetLineColor(2)
+    #distT.Draw()
+    #distF.Draw('same')
+    #c.Print('/home/fabio/Scrivania/match.png')
--- shipLHC/scripts/emulsionTrack.py
+++ shipLHC/scripts/emulsionTrack.py
@@ -1,7 +1,6 @@
 import ROOT,os
 import rootUtils as ut
 import shipunit as u
-import numpy as np
 import ctypes
 
 
@@ -77,26 +76,33 @@ targetRangeY = [10, 60]
 targetRangeZ = [270, 370]
 wallRangeX = [-46, -6]
 wallRangeY = [14, 54]
-fitFile = open("/home/fabio/Simulations/numu_sim_25_July_2022/eml_fits.txt", "w+")
-fitFile.write('{0:<5}  {1:>10}  {2:>10}  {3:>10}  {4:>10}'.format('event','x_bar','x_rad','y_bar','y_rad')+'\n')
-
-def addToFile(addPrint):
-    fitFile.write(addPrint+'\n')
+#fitFile = open("/home/fabio/Simulations/numu_sim_25_July_2022/eml_fits.txt", "w+")
+#fitFile.write('{0:<5}  {1:>10}  {2:>10}  {3:>10}  {4:>10}'.format('event','x_bar','x_rad','y_bar','y_rad')+'\n')
+#
+#def addToFile(addPrint):
+#    fitFile.write(addPrint+'\n')
     
-#def sigPos(Nev = -1):
 Nev = -1
 nBins = 50
 sizeBin = 50/nBins
 binRange = 3
-ut.bookCanvas(h,'eml_map','emulsion map',1200, 1200, cx=1,cy=1)
-ut.bookHist(h, 'eml_xy', '2d emulsion map', nBins, targetRangeX[0], targetRangeX[1], nBins, targetRangeY[0], targetRangeY[1])
+#ut.bookCanvas(h,'eml_map','emulsion map',1200, 1200, cx=1,cy=1)
+#ut.bookHist(h, 'eml_xy', '2d emulsion map', nBins, targetRangeX[0], targetRangeX[1], nBins, targetRangeY[0], targetRangeY[1])
 #ut.bookHist(h, 'eml_z', 'z map', 100, targetRangeZ[0], targetRangeZ[1])
-ut.bookCanvas(h,'res','residuals',1000, 2500, cx=1,cy=3)
-h['res'].SetCanvasSize(1000, 1500)
-h['res'].SetWindowSize(1050, 1200)
-ut.bookHist(h,'res_x','x residuals; x [cm]',100,-10,10)
-ut.bookHist(h,'res_y','y residuals; y [cm]',100,-10,10)
-ut.bookHist(h,'res_d','distance residuals; [cm]',100,0,10)
+#ut.bookCanvas(h,'res','residuals',1000, 2500, cx=1,cy=3)
+#h['res'].SetCanvasSize(1000, 1500)
+#h['res'].SetWindowSize(1050, 1200)
+#ut.bookHist(h,'res_x','x residuals; x [cm]',100,-10,10)
+#ut.bookHist(h,'res_y','y residuals; y [cm]',100,-10,10)
+#ut.bookHist(h,'res_d','distance residuals; [cm]',100,0,10)
+ut.bookCanvas(h, 'map', '2d map', 1800, 1800, 1, 2)
+h['map'].SetCanvasSize(1800, 2400)
+h['map'].SetWindowSize(1850, 1200)
+for i in range(18):
+     for j in range(10):
+          ut.bookHist(h, 'eml_map_'+str(i)+str(j), 'eml_tracks_'+str(i)+str(j), nBins, targetRangeX[0], targetRangeX[1], nBins, targetRangeY[0], targetRangeY[1])
+          ut.bookHist(h, 'scifi_map_'+str(i)+str(j), 'scifi_bar_'+str(i)+str(j), nBins, targetRangeX[0], targetRangeX[1], nBins, targetRangeY[0], targetRangeY[1])
+
 A, B = ROOT.TVector3(), ROOT.TVector3()
 ccCount=0
 inCount = 0
@@ -140,7 +146,7 @@ for event in eventTree:
      if motherWall == -1: continue
      if not cc: continue
      emulsion = 0
-     h['eml_xy'].Reset()
+     #h['eml_xy'].Reset()
      #h['eml_z'].Reset()
      plate60 = '/cave_1/Detector_0/volTarget_1/Wall_{}/Row_0/Brick_0/Emulsion_59'.format(motherWall)
      nav.cd(plate60)
@@ -168,76 +174,115 @@ for event in eventTree:
                if wallRangeX[0] < emlX < wallRangeX[1] and wallRangeY[0] < emlY < wallRangeY[1]:
                     emulsion = 1
                     inCount+=1
-                    h['eml_xy'].Fill(emlX, emlY)
+                    #h['eml_xy'].Fill(emlX, emlY)
+                    blockIndex = int(ccCount/200)
+                    h['eml_map_'+str(blockIndex)+str(ccCount)].Fill(emlX, emlY)
                else:
                     outCount+=1
                     #print('Error: track out of wall', emlX, emlY)
      if not emulsion: continue     
      ccCount+=1
-     h['eml_map'].cd(1)
-     h['eml_xy'].Draw('COLZ')
+     if ccCount > 9:break
+     #h['eml_map'].cd(1)
+     #h['eml_xy'].Draw('COLZ')
      #h['eml_map'].cd(2)
      #h['eml_z'].Draw()
-     h['eml_map'].Update()
-     barX = h['eml_xy'].GetMean(1)
-     barY = h['eml_xy'].GetMean(2)
-     radX = h['eml_xy'].GetStdDev(1)
-     radY = h['eml_xy'].GetStdDev(2)
-     addToFile('{0:<5}  {1:>10}  {2:>10}  {3:>10}  {4:>10}'.format(Nevent, round(barX, 2), round(radX, 2), round(barY, 2), round(radY, 2)))
+     #h['eml_map'].Update()
+     #barX = h['eml_xy'].GetMean(1)
+     #barY = h['eml_xy'].GetMean(2)
+     #radX = h['eml_xy'].GetStdDev(1)
+     #radY = h['eml_xy'].GetStdDev(2)
+     #addToFile('{0:<5}  {1:>10}  {2:>10}  {3:>10}  {4:>10}'.format(Nevent, round(barX, 2), round(radX, 2), round(barY, 2), round(radY, 2)))
      #errMeanX = h['eml_xy'].GetMeanError(1)
      #errMeanY = h['eml_xy'].GetMeanError(2)
 
-     dist = ROOT.TMath.Sqrt((barX-nuX)**2+(barY-nuY)**2)
-     srX += abs((barX-nuX))
-     srY += abs((barY-nuY))
-     srD += dist
-     h['res_x'].Fill(barX-nuX)
-     h['res_y'].Fill(barY-nuY)
-     h['res_d'].Fill(dist)
-
+     #dist = ROOT.TMath.Sqrt((barX-nuX)**2+(barY-nuY)**2)
+     #srX += abs((barX-nuX))
+     #srY += abs((barY-nuY))
+     #srD += dist
+     #h['res_x'].Fill(barX-nuX)
+     #h['res_y'].Fill(barY-nuY)
+     #h['res_d'].Fill(dist)
 
      #rc = input("hit return for next event or q for quit: ")
      #if rc=='q': break
-print("Total number of event: {}".format(eventTree.GetEntries()))
-print("Selected number of event: {}".format(ccCount))
-print("Tracks in/out of target: ", inCount, outCount, outCount/inCount)
 
-print('srX', srX)
-print('srY', srY)
-print('srD', srD)
+scifiCount = 0
+path = '/home/fabio/Simulations/numu_sim_25_July_2022/'
+scifiFile = path+'scifi_fits.txt'
+with open (scifiFile, 'r') as fs:
+     linesScifi = fs.readlines()
+     for lineScifi in linesScifi:
+          lineScifi = lineScifi.replace('\n', '')
+          lineScifi = lineScifi.split()
+          if lineScifi[0] == 'event': continue
+          scifiBarX = float(lineScifi[1])
+          scifiBarY = float(lineScifi[3])
+          scifiIndex = int(scifiCount/200)
+          h['scifi_map_'+str(scifiIndex)+str(scifiCount)].Fill(scifiBarX, scifiBarY)
+          scifiCount+=1
+          if scifiCount > 9: break
 
-h['res'].cd(1)
-fitResX = h['res_x'].Fit('gaus','SQ','',-20,20)
-fitResY = h['res_y'].Fit('gaus','SQ','',-20,20)
-resMeanX = fitResX.Parameter(1)
-resVarX = fitResX.Parameter(2)
-resMeanY = fitResY.Parameter(1)
-resVarY = fitResY.Parameter(2)
+for i in range(1):
+     #h['eml_map_'+str(i)].SetFillColor(4)
+     #h['scifi_map_'+str(i)].SetFillColor(2)
+     for j in range(10):
+          h['eml_map_'+str(i)+str(j)].SetMarkerStyle(20)
+          h['scifi_map_'+str(i)+str(j)].SetMarkerStyle(22)
+          if j != 10:
+               h['eml_map_'+str(i)+str(j)].SetMarkerColor(j+1)
+               h['scifi_map_'+str(i)+str(j)].SetMarkerColor(j+1)
+          else:
+               h['eml_map_'+str(i)+str(j)].SetMarkerColor(j+2)
+               h['scifi_map_'+str(i)+str(j)].SetMarkerColor(j+2)
+          h['eml_map_'+str(i)+str(j)].SetMarkerSize(0.8)
+          h['scifi_map_'+str(i)+str(j)].SetMarkerSize(1.5)
+          h['eml_map_'+str(i)+str(j)].SetStats(False)
+          h['scifi_map_'+str(i)+str(j)].SetStats(False)
+          h['map'].cd(i+1)
+          h['eml_map_'+str(i)+str(j)].Draw('same')
+          h['scifi_map_'+str(i)+str(j)].Draw('same')
+     h['map'].cd(i+1).BuildLegend(0.75, 0.85, 0.95, 0.95)
+h['map'].Print('/home/fabio/Scrivania/2dmap_1.png')
+#print("Total number of event: {}".format(eventTree.GetEntries()))
+#print("Selected number of event: {}".format(ccCount))
+#print("Tracks in/out of target: ", inCount, outCount, outCount/(inCount+outCount))
+#
+#print('srX', srX)
+#print('srY', srY)
+#print('srD', srD)
+#
+#h['res'].cd(1)
+#fitResX = h['res_x'].Fit('gaus','SQ','',-20,20)
+#fitResY = h['res_y'].Fit('gaus','SQ','',-20,20)
+#resMeanX = fitResX.Parameter(1)
+#resVarX = fitResX.Parameter(2)
+#resMeanY = fitResY.Parameter(1)
+#resVarY = fitResY.Parameter(2)
 #print(resMeanX, resVarX, resMeanY, resVarY)
-eff0x = 0
-eff0y = 0
-eff1x = h['res_x'].GetEntries()
-eff1y = h['res_x'].GetEntries()
-for binX in range(h['res_x'].GetXaxis().GetNbins()):
-     if h['res_x'].GetBinCenter(binX) < resMeanX - 3 * resVarX:
-          eff0x += h['res_x'].GetBinContent(binX)
-     elif h['res_x'].GetBinCenter(binX) > resMeanX + 3 * resVarX:
-          eff0x += h['res_x'].GetBinContent(binX)
-for binY in range(h['res_y'].GetXaxis().GetNbins()):
-     if h['res_y'].GetBinCenter(binY) < resMeanY - 3 * resVarY:
-          eff0y += h['res_y'].GetBinContent(binY)
-     elif h['res_y'].GetBinCenter(binY) > resMeanY + 3 * resVarY:
-          eff0y += h['res_y'].GetBinContent(binY)
-effx = 1-eff0x/eff1x
-effy = 1-eff0y/eff1y
-print('efficiency x:', effx, eff0x, eff1x)
-print('efficiency y:', effy, eff0y, eff1y)
-h['res_x'].Draw()
-h['res'].cd(2)
-h['res_y'].Draw()
-h['res'].cd(3)
-h['res_d'].Draw()
-h['res'].Print('/home/fabio/Simulations/numu_sim_25_July_2022/eml_res_3.png')
-
-fitFile.close()
-#sigPos()
+#eff0x = 0
+#eff0y = 0
+#eff1x = h['res_x'].GetEntries()
+#eff1y = h['res_y'].GetEntries()
+#for binX in range(h['res_x'].GetXaxis().GetNbins()):
+#     if h['res_x'].GetBinCenter(binX) < resMeanX - 3 * resVarX:
+#          eff0x += h['res_x'].GetBinContent(binX)
+#     elif h['res_x'].GetBinCenter(binX) > resMeanX + 3 * resVarX:
+#          eff0x += h['res_x'].GetBinContent(binX)
+#for binY in range(h['res_y'].GetXaxis().GetNbins()):
+#     if h['res_y'].GetBinCenter(binY) < resMeanY - 3 * resVarY:
+#          eff0y += h['res_y'].GetBinContent(binY)
+#     elif h['res_y'].GetBinCenter(binY) > resMeanY + 3 * resVarY:
+#          eff0y += h['res_y'].GetBinContent(binY)
+#effx = 1-eff0x/eff1x
+#effy = 1-eff0y/eff1y
+#print('efficiency x:', effx, eff0x, eff1x)
+#print('efficiency y:', effy, eff0y, eff1y)
+#h['res_x'].Draw()
+#h['res'].cd(2)
+#h['res_y'].Draw()
+#h['res'].cd(3)
+#h['res_d'].Draw()
+#h['res'].Print('/home/fabio/Simulations/numu_sim_25_July_2022/eml_res_3.png')
+#
+#fitFile.close()
